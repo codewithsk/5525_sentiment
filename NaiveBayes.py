6f6f3aaca325e5986cd297a9eaf43e94865a5b6a
Alan Ritter <aritter@Alans-MacBook-Pro.local>
2019-01-08 15:09:46 -0500

    added license

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,3 +1,13 @@
+# NaiveBayes.py
+# --------------
+# Licensing Information:  You are free to use or extend this project for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to The Ohio State University, including a link to http://aritter.github.io/courses/5525_spring19.html
+#
+# Attribution Information: This assignment was developed at The Ohio State University
+# by Alan Ritter (ritter.1492@osu.edu).
+
 import sys
 
 import numpy as np
